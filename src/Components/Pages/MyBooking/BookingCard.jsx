fd0824b6a9963a19134e58e1269e12a13e64f8a7
skashequr <learningwithextracare@gmail.com>
2023-11-10 06:59:46 +0600

    you cant delited befor one day

--- src/Components/Pages/MyBooking/BookingCard.jsx
+++ src/Components/Pages/MyBooking/BookingCard.jsx
@@ -1,16 +1,44 @@
-
 import { GiCancel } from "react-icons/gi";
 import { Link } from "react-router-dom";
-
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 const BookingCard = ({ booked, handleCancelBooking }) => {
   // console.log(booked);
 
   // console.log(user);
-  const { _id, name, pricePerNight, startDate , roomSize , endDate , description, specialOffers, img1, date } =
-    booked || {};
-  
+  const {
+    _id,
+    name,
+    pricePerNight,
+    startDate,
+    roomSize,
+    endDate,
+    description,
+    specialOffers,
+    img1,
+    date,
+  } = booked || {};
 
+  function canCancelBooking(bookingDate) {
+    // Set the cancellation period to 1 day
+    const cancellationPeriod = 1;
 
+    // Get the current date
+    const currentDate = new Date();
+
+    // Calculate the difference in milliseconds between the booking date and the current date
+    const timeDifference = bookingDate - currentDate;
+
+    // Calculate the difference in days
+    const daysDifference = timeDifference / (1000 * 60 * 60 * 24);
+
+    // Check if the booking can be canceled
+    return daysDifference >= cancellationPeriod;
+  }
+  const bookingDate = new Date(startDate);
+
+  const canCancel = canCancelBooking(bookingDate);
+  console.log(bookingDate, canCancel);
   return (
     <div className="mt-5">
       <div className="card card-side bg-base-100 shadow-xl">
@@ -19,21 +47,30 @@ const BookingCard = ({ booked, handleCancelBooking }) => {
         </figure>
         <div className="card-body">
           <div className="card-actions justify-end">
-          <Link to={`/bookingRooms/updateDate/${_id}`}><button className="btn btn-primary">Update Date</button></Link> 
+            <Link to={`/bookingRooms/updateDate/${_id}`}>
+              <button className="btn btn-primary">Update Date</button>
+            </Link>
           </div>
           <h2 className="card-title">{name}</h2>
           <p>{description}</p>
           <p>{date}</p>
-          <p>{startDate} to {endDate}</p>
+          <p>
+            {startDate} to {endDate}
+          </p>
           <p>{roomSize}</p>
           <div className="card-actions justify-end ">
-            <button onClick={() => handleCancelBooking(_id)}>
+            <button
+              onClick={
+                canCancel
+                  ? () => handleCancelBooking(_id)
+                  : () => toast.error("Cannot cancel booking at this time.")
+              }
+            >
               <GiCancel className="h-10 w-10" />
             </button>
           </div>
         </div>
       </div>
-      
     </div>
   );
 };
