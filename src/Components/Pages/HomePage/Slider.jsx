ac3573dba404d01b18b93272d3c4167a807d0f30
skashequr <learningwithextracare@gmail.com>
2023-11-11 22:17:53 +0600

    Solve some isu

--- src/Components/Pages/HomePage/Slider.jsx
+++ src/Components/Pages/HomePage/Slider.jsx
@@ -3,7 +3,7 @@ import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
+
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
