5292cffc0d2520cec3414a9cdb199e211dcc09c9
skashequr <learningwithextracare@gmail.com>
2023-11-11 21:49:33 +0600

    Home Page responce Done

--- src/Components/Pages/HomePage/MapComponent.jsx
+++ src/Components/Pages/HomePage/MapComponent.jsx
@@ -3,10 +3,10 @@ import { MyMap } from './MyMap';
 import { FiMapPin } from "react-icons/fi";
 const MapComponent = () => {
     return (
-        <div>
+        <div >
       <p className='text-center text-5xl font-bold leading-none tracking-tighter text-neutral-600 mt-10'>Our location </p>
 
-            <div className='flex m-10'>
+            <div className='grid grid-cols-1 mb-6 lg:grid-cols-2 mt-10'>
                 <div>
                 <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="max-w-lg sm:text-center sm:mx-auto">
